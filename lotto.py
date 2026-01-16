ac96a5a3dc9509146b9f278d72c328294137ad80
geniethorzzang <geniethorzzang@gmail.com>
2026-01-16 10:10:35 +0900

    Update lotto.py

--- lotto.py
+++ lotto.py
@@ -18,7 +18,12 @@ button = st.button("로또 번호 생성해주세요!")
 
 
 
+
+
+
 if button :
     for i in range(1,6) :
         st.subheader(f"{i}번째 추천 로또 번호: {generate_lotto()}")
     st.write(f"생성된 시각: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+
+    
